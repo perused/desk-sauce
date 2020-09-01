30c65093e909048f15e48eef088baee8895ca8b4
Sam <sjac6722@uni.sydney.edu.au>
2020-09-01 17:02:16 +1000

    this is a test

--- run.sh
+++ run.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
-python3 test.py
-python3 main.py
-rmdir /home/sam/Desktop/Cleaned
+# python3 test.py
+# python3 main.py
+# rmdir /home/sam/Desktop/Cleaned
+
+if [ $1 = g ]
+then
+  if [ "$2" ]
+  then
+    git add .
+    git commit -m "$2"
+    git push
+  else
+    echo Please specify commit message
+  fi
+fi
